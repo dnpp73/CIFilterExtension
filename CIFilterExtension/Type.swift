df649d1a95f7cf63de3e652f97cbbfe3ad52e213
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2021-10-09 21:03:16 +0900

    mutating types

--- CIFilterExtension/Type.swift
+++ CIFilterExtension/Type.swift
@@ -1,3 +1,5 @@
+// swiftlint:disable multiline_parameters
+
 import Foundation
 import CoreImage
 
@@ -47,84 +49,95 @@ internal func composer(name: String) -> ImageComposer {
 // MARK: - Public Structs
 
 public struct RGBAComponents {
-    public let r: CGFloat
-    public let g: CGFloat
-    public let b: CGFloat
-    public let a: CGFloat
+    public private(set) var vector: CIVector
+    public var r: CGFloat { didSet { updateVector() } }
+    public var g: CGFloat { didSet { updateVector() } }
+    public var b: CGFloat { didSet { updateVector() } }
+    public var a: CGFloat { didSet { updateVector() } }
     public init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
         self.r = r
         self.g = g
         self.b = b
         self.a = a
+        vector = CIVector(x: r, y: g, z: b, w: a)
     }
-    public var vector: CIVector {
-        CIVector(x: r, y: g, z: b, w: a)
+    private mutating func updateVector() {
+        vector = CIVector(x: r, y: g, z: b, w: a)
     }
 }
 
 public struct TempAndTint {
-    public let temp: CGFloat
-    public let tint: CGFloat
+    public private(set) var vector: CIVector
+    public var temp: CGFloat { didSet { updateVector() } }
+    public var tint: CGFloat { didSet { updateVector() } }
     public init(temp: CGFloat, tint: CGFloat) {
         self.temp = temp
         self.tint = tint
+        vector = CIVector(x: temp, y: tint)
     }
-    public var vector: CIVector {
-        CIVector(x: temp, y: tint)
+    private mutating func updateVector() {
+        vector = CIVector(x: temp, y: tint)
     }
 }
 
 public struct XYPosition {
-    public let x: CGFloat
-    public let y: CGFloat
+    public private(set) var vector: CIVector
+    public var x: CGFloat { didSet { updateVector() } }
+    public var y: CGFloat { didSet { updateVector() } }
     public init(x: CGFloat, y: CGFloat) {
         self.x = x
         self.y = y
+        vector = CIVector(x: x, y: y)
     }
-    public var vector: CIVector {
-        CIVector(x: x, y: y)
+    private mutating func updateVector() {
+        vector = CIVector(x: x, y: y)
     }
 }
 
 public struct XYOffset {
-    public let x: CGFloat
-    public let y: CGFloat
+    public private(set) var vector: CIVector
+    public var x: CGFloat { didSet { updateVector() } }
+    public var y: CGFloat { didSet { updateVector() } }
     public init(x: CGFloat, y: CGFloat) {
         self.x = x
         self.y = y
+        vector = CIVector(x: x, y: y)
     }
-    public var vector: CIVector {
-        CIVector(x: x, y: y)
+    private mutating func updateVector() {
+        vector = CIVector(x: x, y: y)
     }
 }
 
 public struct PolynomialCoefficients {
-    public let a0: CGFloat
-    public let a1: CGFloat
-    public let a2: CGFloat
-    public let a3: CGFloat
+    public private(set) var vector: CIVector
+    public var a0: CGFloat { didSet { updateVector() } }
+    public var a1: CGFloat { didSet { updateVector() } }
+    public var a2: CGFloat { didSet { updateVector() } }
+    public var a3: CGFloat { didSet { updateVector() } }
     public init(a0: CGFloat, a1: CGFloat, a2: CGFloat, a3: CGFloat) {
         self.a0 = a0
         self.a1 = a1
         self.a2 = a2
         self.a3 = a3
+        vector = CIVector(x: a0, y: a1, z: a2, w: a3)
     }
-    public var vector: CIVector {
-        CIVector(x: a0, y: a1, z: a2, w: a3)
+    private mutating func updateVector() {
+        vector = CIVector(x: a0, y: a1, z: a2, w: a3)
     }
 }
 
 public struct CrossPolynomialCoefficients {
-    public let a0: CGFloat
-    public let a1: CGFloat
-    public let a2: CGFloat
-    public let a3: CGFloat
-    public let a4: CGFloat
-    public let a5: CGFloat
-    public let a6: CGFloat
-    public let a7: CGFloat
-    public let a8: CGFloat
-    public let a9: CGFloat
+    public private(set) var vector: CIVector
+    public var a0: CGFloat { didSet { updateVector() } }
+    public var a1: CGFloat { didSet { updateVector() } }
+    public var a2: CGFloat { didSet { updateVector() } }
+    public var a3: CGFloat { didSet { updateVector() } }
+    public var a4: CGFloat { didSet { updateVector() } }
+    public var a5: CGFloat { didSet { updateVector() } }
+    public var a6: CGFloat { didSet { updateVector() } }
+    public var a7: CGFloat { didSet { updateVector() } }
+    public var a8: CGFloat { didSet { updateVector() } }
+    public var a9: CGFloat { didSet { updateVector() } }
     public init(a0: CGFloat, a1: CGFloat, a2: CGFloat, a3: CGFloat, a4: CGFloat, a5: CGFloat, a6: CGFloat, a7: CGFloat, a8: CGFloat, a9: CGFloat) {
         self.a0 = a0
         self.a1 = a1
@@ -136,22 +149,24 @@ public struct CrossPolynomialCoefficients {
         self.a7 = a7
         self.a8 = a8
         self.a9 = a9
+        vector = CIVector(string: "[\(a0) \(a1) \(a2) \(a3) \(a4) \(a5) \(a6) \(a7) \(a8) \(a9)]")
     }
-    public var vector: CIVector {
-        CIVector(string: "[\(a0) \(a1) \(a2) \(a3) \(a4) \(a5) \(a6) \(a7) \(a8) \(a9)]")
+    private mutating func updateVector() {
+        vector = CIVector(string: "[\(a0) \(a1) \(a2) \(a3) \(a4) \(a5) \(a6) \(a7) \(a8) \(a9)]")
     }
 }
 
 public struct ConvolutionMatrix3X3 {
-    public let w00: CGFloat
-    public let w01: CGFloat
-    public let w02: CGFloat
-    public let w10: CGFloat
-    public let w11: CGFloat
-    public let w12: CGFloat
-    public let w20: CGFloat
-    public let w21: CGFloat
-    public let w22: CGFloat
+    public private(set) var vector: CIVector
+    public var w00: CGFloat { didSet { updateVector() } }
+    public var w01: CGFloat { didSet { updateVector() } }
+    public var w02: CGFloat { didSet { updateVector() } }
+    public var w10: CGFloat { didSet { updateVector() } }
+    public var w11: CGFloat { didSet { updateVector() } }
+    public var w12: CGFloat { didSet { updateVector() } }
+    public var w20: CGFloat { didSet { updateVector() } }
+    public var w21: CGFloat { didSet { updateVector() } }
+    public var w22: CGFloat { didSet { updateVector() } }
     public init(w00: CGFloat, w01: CGFloat, w02: CGFloat,
                 w10: CGFloat, w11: CGFloat, w12: CGFloat,
                 w20: CGFloat, w21: CGFloat, w22: CGFloat) {
@@ -164,38 +179,40 @@ public struct ConvolutionMatrix3X3 {
         self.w20 = w20
         self.w21 = w21
         self.w22 = w22
+        vector = CIVector(string: "[\(w00) \(w01) \(w02) \(w10) \(w11) \(w12) \(w20) \(w21) \(w22)]")
     }
-    public var vector: CIVector {
-        CIVector(string: "[\(w00) \(w01) \(w02) \(w10) \(w11) \(w12) \(w20) \(w21) \(w22)]")
+    private mutating func updateVector() {
+        vector = CIVector(string: "[\(w00) \(w01) \(w02) \(w10) \(w11) \(w12) \(w20) \(w21) \(w22)]")
     }
 }
 
 public struct ConvolutionMatrix5X5 {
-    public let w00: CGFloat
-    public let w01: CGFloat
-    public let w02: CGFloat
-    public let w03: CGFloat
-    public let w04: CGFloat
-    public let w10: CGFloat
-    public let w11: CGFloat
-    public let w12: CGFloat
-    public let w13: CGFloat
-    public let w14: CGFloat
-    public let w20: CGFloat
-    public let w21: CGFloat
-    public let w22: CGFloat
-    public let w23: CGFloat
-    public let w24: CGFloat
-    public let w30: CGFloat
-    public let w31: CGFloat
-    public let w32: CGFloat
-    public let w33: CGFloat
-    public let w34: CGFloat
-    public let w40: CGFloat
-    public let w41: CGFloat
-    public let w42: CGFloat
-    public let w43: CGFloat
-    public let w44: CGFloat
+    public private(set) var vector: CIVector
+    public var w00: CGFloat { didSet { updateVector() } }
+    public var w01: CGFloat { didSet { updateVector() } }
+    public var w02: CGFloat { didSet { updateVector() } }
+    public var w03: CGFloat { didSet { updateVector() } }
+    public var w04: CGFloat { didSet { updateVector() } }
+    public var w10: CGFloat { didSet { updateVector() } }
+    public var w11: CGFloat { didSet { updateVector() } }
+    public var w12: CGFloat { didSet { updateVector() } }
+    public var w13: CGFloat { didSet { updateVector() } }
+    public var w14: CGFloat { didSet { updateVector() } }
+    public var w20: CGFloat { didSet { updateVector() } }
+    public var w21: CGFloat { didSet { updateVector() } }
+    public var w22: CGFloat { didSet { updateVector() } }
+    public var w23: CGFloat { didSet { updateVector() } }
+    public var w24: CGFloat { didSet { updateVector() } }
+    public var w30: CGFloat { didSet { updateVector() } }
+    public var w31: CGFloat { didSet { updateVector() } }
+    public var w32: CGFloat { didSet { updateVector() } }
+    public var w33: CGFloat { didSet { updateVector() } }
+    public var w34: CGFloat { didSet { updateVector() } }
+    public var w40: CGFloat { didSet { updateVector() } }
+    public var w41: CGFloat { didSet { updateVector() } }
+    public var w42: CGFloat { didSet { updateVector() } }
+    public var w43: CGFloat { didSet { updateVector() } }
+    public var w44: CGFloat { didSet { updateVector() } }
     public init(w00: CGFloat, w01: CGFloat, w02: CGFloat, w03: CGFloat, w04: CGFloat,
                 w10: CGFloat, w11: CGFloat, w12: CGFloat, w13: CGFloat, w14: CGFloat,
                 w20: CGFloat, w21: CGFloat, w22: CGFloat, w23: CGFloat, w24: CGFloat,
@@ -226,62 +243,64 @@ public struct ConvolutionMatrix5X5 {
         self.w42 = w42
         self.w43 = w43
         self.w44 = w44
+        vector = CIVector(string: "[\(w00) \(w01) \(w02) \(w03) \(w04) \(w10) \(w11) \(w12) \(w13) \(w14) \(w20) \(w21) \(w22) \(w23) \(w24) \(w30) \(w31) \(w32) \(w33) \(w34) \(w40) \(w41) \(w42) \(w43) \(w44)]")
     }
-    public var vector: CIVector {
-        CIVector(string: "[\(w00) \(w01) \(w02) \(w03) \(w04) \(w10) \(w11) \(w12) \(w13) \(w14) \(w20) \(w21) \(w22) \(w23) \(w24) \(w30) \(w31) \(w32) \(w33) \(w34) \(w40) \(w41) \(w42) \(w43) \(w44)]")
+    private mutating func updateVector() {
+        vector = CIVector(string: "[\(w00) \(w01) \(w02) \(w03) \(w04) \(w10) \(w11) \(w12) \(w13) \(w14) \(w20) \(w21) \(w22) \(w23) \(w24) \(w30) \(w31) \(w32) \(w33) \(w34) \(w40) \(w41) \(w42) \(w43) \(w44)]")
     }
 }
 
 public struct ConvolutionMatrix7X7 {
-    public let w00: CGFloat
-    public let w01: CGFloat
-    public let w02: CGFloat
-    public let w03: CGFloat
-    public let w04: CGFloat
-    public let w05: CGFloat
-    public let w06: CGFloat
-    public let w10: CGFloat
-    public let w11: CGFloat
-    public let w12: CGFloat
-    public let w13: CGFloat
-    public let w14: CGFloat
-    public let w15: CGFloat
-    public let w16: CGFloat
-    public let w20: CGFloat
-    public let w21: CGFloat
-    public let w22: CGFloat
-    public let w23: CGFloat
-    public let w24: CGFloat
-    public let w25: CGFloat
-    public let w26: CGFloat
-    public let w30: CGFloat
-    public let w31: CGFloat
-    public let w32: CGFloat
-    public let w33: CGFloat
-    public let w34: CGFloat
-    public let w35: CGFloat
-    public let w36: CGFloat
-    public let w40: CGFloat
-    public let w41: CGFloat
-    public let w42: CGFloat
-    public let w43: CGFloat
-    public let w44: CGFloat
-    public let w45: CGFloat
-    public let w46: CGFloat
-    public let w50: CGFloat
-    public let w51: CGFloat
-    public let w52: CGFloat
-    public let w53: CGFloat
-    public let w54: CGFloat
-    public let w55: CGFloat
-    public let w56: CGFloat
-    public let w60: CGFloat
-    public let w61: CGFloat
-    public let w62: CGFloat
-    public let w63: CGFloat
-    public let w64: CGFloat
-    public let w65: CGFloat
-    public let w66: CGFloat
+    public private(set) var vector: CIVector
+    public var w00: CGFloat { didSet { updateVector() } }
+    public var w01: CGFloat { didSet { updateVector() } }
+    public var w02: CGFloat { didSet { updateVector() } }
+    public var w03: CGFloat { didSet { updateVector() } }
+    public var w04: CGFloat { didSet { updateVector() } }
+    public var w05: CGFloat { didSet { updateVector() } }
+    public var w06: CGFloat { didSet { updateVector() } }
+    public var w10: CGFloat { didSet { updateVector() } }
+    public var w11: CGFloat { didSet { updateVector() } }
+    public var w12: CGFloat { didSet { updateVector() } }
+    public var w13: CGFloat { didSet { updateVector() } }
+    public var w14: CGFloat { didSet { updateVector() } }
+    public var w15: CGFloat { didSet { updateVector() } }
+    public var w16: CGFloat { didSet { updateVector() } }
+    public var w20: CGFloat { didSet { updateVector() } }
+    public var w21: CGFloat { didSet { updateVector() } }
+    public var w22: CGFloat { didSet { updateVector() } }
+    public var w23: CGFloat { didSet { updateVector() } }
+    public var w24: CGFloat { didSet { updateVector() } }
+    public var w25: CGFloat { didSet { updateVector() } }
+    public var w26: CGFloat { didSet { updateVector() } }
+    public var w30: CGFloat { didSet { updateVector() } }
+    public var w31: CGFloat { didSet { updateVector() } }
+    public var w32: CGFloat { didSet { updateVector() } }
+    public var w33: CGFloat { didSet { updateVector() } }
+    public var w34: CGFloat { didSet { updateVector() } }
+    public var w35: CGFloat { didSet { updateVector() } }
+    public var w36: CGFloat { didSet { updateVector() } }
+    public var w40: CGFloat { didSet { updateVector() } }
+    public var w41: CGFloat { didSet { updateVector() } }
+    public var w42: CGFloat { didSet { updateVector() } }
+    public var w43: CGFloat { didSet { updateVector() } }
+    public var w44: CGFloat { didSet { updateVector() } }
+    public var w45: CGFloat { didSet { updateVector() } }
+    public var w46: CGFloat { didSet { updateVector() } }
+    public var w50: CGFloat { didSet { updateVector() } }
+    public var w51: CGFloat { didSet { updateVector() } }
+    public var w52: CGFloat { didSet { updateVector() } }
+    public var w53: CGFloat { didSet { updateVector() } }
+    public var w54: CGFloat { didSet { updateVector() } }
+    public var w55: CGFloat { didSet { updateVector() } }
+    public var w56: CGFloat { didSet { updateVector() } }
+    public var w60: CGFloat { didSet { updateVector() } }
+    public var w61: CGFloat { didSet { updateVector() } }
+    public var w62: CGFloat { didSet { updateVector() } }
+    public var w63: CGFloat { didSet { updateVector() } }
+    public var w64: CGFloat { didSet { updateVector() } }
+    public var w65: CGFloat { didSet { updateVector() } }
+    public var w66: CGFloat { didSet { updateVector() } }
     public init(w00: CGFloat, w01: CGFloat, w02: CGFloat, w03: CGFloat, w04: CGFloat, w05: CGFloat, w06: CGFloat,
                 w10: CGFloat, w11: CGFloat, w12: CGFloat, w13: CGFloat, w14: CGFloat, w15: CGFloat, w16: CGFloat,
                 w20: CGFloat, w21: CGFloat, w22: CGFloat, w23: CGFloat, w24: CGFloat, w25: CGFloat, w26: CGFloat,
@@ -338,22 +357,24 @@ public struct ConvolutionMatrix7X7 {
         self.w64 = w64
         self.w65 = w65
         self.w66 = w66
+        vector = CIVector(string: "[\(w00) \(w01) \(w02) \(w03) \(w04) \(w05) \(w06) \(w10) \(w11) \(w12) \(w13) \(w14) \(w15) \(w16) \(w20) \(w21) \(w22) \(w23) \(w24) \(w25) \(w26) \(w30) \(w31) \(w32) \(w33) \(w34) \(w35) \(w36) \(w40) \(w41) \(w42) \(w43) \(w44) \(w45) \(w46) \(w50) \(w51) \(w52) \(w53) \(w54) \(w55) \(w56) \(w60) \(w61) \(w62) \(w63) \(w64) \(w65) \(w66)]")
     }
-    public var vector: CIVector {
-        CIVector(string: "[\(w00) \(w01) \(w02) \(w03) \(w04) \(w05) \(w06) \(w10) \(w11) \(w12) \(w13) \(w14) \(w15) \(w16) \(w20) \(w21) \(w22) \(w23) \(w24) \(w25) \(w26) \(w30) \(w31) \(w32) \(w33) \(w34) \(w35) \(w36) \(w40) \(w41) \(w42) \(w43) \(w44) \(w45) \(w46) \(w50) \(w51) \(w52) \(w53) \(w54) \(w55) \(w56) \(w60) \(w61) \(w62) \(w63) \(w64) \(w65) \(w66)]")
+    private mutating func updateVector() {
+        vector = CIVector(string: "[\(w00) \(w01) \(w02) \(w03) \(w04) \(w05) \(w06) \(w10) \(w11) \(w12) \(w13) \(w14) \(w15) \(w16) \(w20) \(w21) \(w22) \(w23) \(w24) \(w25) \(w26) \(w30) \(w31) \(w32) \(w33) \(w34) \(w35) \(w36) \(w40) \(w41) \(w42) \(w43) \(w44) \(w45) \(w46) \(w50) \(w51) \(w52) \(w53) \(w54) \(w55) \(w56) \(w60) \(w61) \(w62) \(w63) \(w64) \(w65) \(w66)]")
     }
 }
 
 public struct ConvolutionVector9 {
-    public let w0: CGFloat
-    public let w1: CGFloat
-    public let w2: CGFloat
-    public let w3: CGFloat
-    public let w4: CGFloat
-    public let w5: CGFloat
-    public let w6: CGFloat
-    public let w7: CGFloat
-    public let w8: CGFloat
+    public private(set) var vector: CIVector
+    public var w0: CGFloat { didSet { updateVector() } }
+    public var w1: CGFloat { didSet { updateVector() } }
+    public var w2: CGFloat { didSet { updateVector() } }
+    public var w3: CGFloat { didSet { updateVector() } }
+    public var w4: CGFloat { didSet { updateVector() } }
+    public var w5: CGFloat { didSet { updateVector() } }
+    public var w6: CGFloat { didSet { updateVector() } }
+    public var w7: CGFloat { didSet { updateVector() } }
+    public var w8: CGFloat { didSet { updateVector() } }
     public init(w0: CGFloat, w1: CGFloat, w2: CGFloat, w3: CGFloat, w4: CGFloat, w5: CGFloat, w6: CGFloat, w7: CGFloat, w8: CGFloat) {
         self.w0 = w0
         self.w1 = w1
@@ -364,8 +385,9 @@ public struct ConvolutionVector9 {
         self.w6 = w6
         self.w7 = w7
         self.w8 = w8
+        vector = CIVector(string: "[\(w0) \(w1) \(w2) \(w3) \(w4) \(w5) \(w6) \(w7) \(w8)]")
     }
-    public var vector: CIVector {
-        CIVector(string: "[\(w0) \(w1) \(w2) \(w3) \(w4) \(w5) \(w6) \(w7) \(w8)]")
+    private mutating func updateVector() {
+        vector = CIVector(string: "[\(w0) \(w1) \(w2) \(w3) \(w4) \(w5) \(w6) \(w7) \(w8)]")
     }
 }
